f3f4515e22299606fcb111a69c2b817a774be626
Sunny Cao <sunnynz2016@gmail.com>
2020-07-28 10:25:32 +1200

    Add swagger

--- CreditDemo/CreditDemo/Startup.cs
+++ CreditDemo/CreditDemo/Startup.cs
@@ -22,6 +22,9 @@ namespace CreditDemo
         {
             services.AddDbContext<CreditCardContext>(opt => opt.UseInMemoryDatabase("CreditCardInfo"));
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,7 +36,6 @@ namespace CreditDemo
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -42,6 +44,17 @@ namespace CreditDemo
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = "swagger";
             });
         }
     }
