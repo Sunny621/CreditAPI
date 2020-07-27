2b6b1fd71c2331de81ff7e8de19f1f712cc9d915
Sunny Cao <sunnynz2016@gmail.com>
2020-07-28 09:19:28 +1200

    Delete useless folders and add credit models

---
+++ CreditDemo/CreditDemo/Models/CreditCardContext.cs
@@ -0,0 +1,15 @@
+﻿using CreditDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CreditCard.Models
+{
+    public class CreditCardContext : DbContext
+    {
+        public CreditCardContext(DbContextOptions<CreditCardContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CreditCardItem> CreditCardItems { get; set; }
+
+    }
+}
---
+++ CreditDemo/CreditDemo/Models/CreditCardItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CreditDemo.Models
+{
+    public class CreditCardItem
+    {
+        public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "Name is longer than 50")]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(100000000000, 9999999999999999999, ErrorMessage = "Credit card number must be between 12 and 19 digits")]
+        public long CreditCard { get; set; }
+
+        [RegularExpression("(^[0-9]{3,4}$)", ErrorMessage = "Please enter valid Number for CVC")]
+        [Required]
+        public int CVC { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "Please enter valid Date")]
+        [Required]
+        public DateTime ExpiryDate { get; set; }
+
+    }
+}
