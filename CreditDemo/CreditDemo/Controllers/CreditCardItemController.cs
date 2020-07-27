1f4e790e1630a88f17197f52a97e97e281b356f1
Sunny Cao <sunnynz2016@gmail.com>
2020-07-28 10:17:22 +1200

    Add controller

---
+++ CreditDemo/CreditDemo/Controllers/CreditCardItemController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CreditCard.Models;
+using CreditDemo.Models;
+using Microsoft.Extensions.Logging;
+
+namespace CreditDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreditCardItemController : ControllerBase
+    {
+        private readonly CreditCardContext _context;
+        private readonly ILogger<CreditCardItemController> _logger;
+
+
+        public CreditCardItemController(CreditCardContext context, ILogger<CreditCardItemController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/CreditCardItem
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CreditCardItem>>> GetCreditCardItems()
+        {
+            return await _context.CreditCardItems.ToListAsync();
+        }
+
+        // GET: api/CreditCardItem/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CreditCardItem>> GetCreditCardItem(int id)
+        {
+            var creditCardItem = await _context.CreditCardItems.FindAsync(id);
+            _logger.LogInformation($"Get credit card info for id: {id}");
+            if (creditCardItem == null)
+            {
+                _logger.LogDebug($"No credit card info found for id: {id}");
+                return NotFound();
+            }
+            return creditCardItem;
+        }
+
+        // PUT: api/CreditCardItem/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCreditCardItem(int id, CreditCardItem creditCardItem)
+        {
+            _logger.LogInformation($"Modify credit card info for id: {id}");
+
+            if (!ModelState.IsValid)
+            {
+                _logger.LogDebug("Invalid data.");
+                return BadRequest("Invalid data.");
+            }
+
+            if (id != creditCardItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(creditCardItem).State = EntityState.Modified;
+
+            try
+            {
+
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CreditCardItemExists(id))
+                {
+                    _logger.LogDebug($"Can't find credit card info for id : {id}");
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+
+            return NoContent();
+        }
+
+        // POST: api/CreditCardItem
+        [HttpPost]
+        public async Task<ActionResult<CreditCardItem>> PostCreditCardItem(CreditCardItem creditCardItem)
+        {
+            _logger.LogInformation("Create credit card info");
+            if (!ModelState.IsValid)
+            {
+                _logger.LogDebug("Invalid data.");
+                return BadRequest("Invalid data.");
+            }
+            _context.CreditCardItems.Add(creditCardItem);
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+        }
+
+        // DELETE: api/CreditCardItem/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<CreditCardItem>> DeleteCreditCardItem(int id)
+        {
+            _logger.LogInformation($"Delete credit card info for {id}");
+            var creditCardItem = await _context.CreditCardItems.FindAsync(id);
+            if (creditCardItem == null)
+            {
+                _logger.LogDebug($"Can't find credit card info for id : {id}");
+                return NotFound();
+            }
+
+            _context.CreditCardItems.Remove(creditCardItem);
+            await _context.SaveChangesAsync();
+
+            return creditCardItem;
+        }
+
+        private bool CreditCardItemExists(int id)
+        {
+            return _context.CreditCardItems.Any(e => e.Id == id);
+        }
+
+    }
+}
--- CreditDemo/CreditDemo/Startup.cs
+++ CreditDemo/CreditDemo/Startup.cs
@@ -21,6 +21,7 @@ namespace CreditDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<CreditCardContext>(opt => opt.UseInMemoryDatabase("CreditCardInfo"));
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,12 +31,7 @@ namespace CreditDemo
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -45,9 +41,7 @@ namespace CreditDemo
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
             });
         }
     }
